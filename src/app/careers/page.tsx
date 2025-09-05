c714a16b4f9d056d532104c635d56c3d21116863
Andrien Wiandyano <arnnied03@gmail.com>
2025-09-05 21:31:25 +0700

    feat: update metadata for careers page

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -2,7 +2,34 @@ import CareersPage from '@/pages/CareersPage';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
+  alternates: {
+    canonical: '/careers',
+  },
   title: 'Careers',
+  authors: [{ name: 'Andrien Wiandyano', url: 'https://andrienw.com' }],
+  description:
+    "Andrien Wiandyano's life journey, education, and professional experience.",
+  keywords: [
+    'Andrien Wiandyano',
+    'Andrien',
+    'Wiandyano',
+    'AndrienW',
+    'Portfolio',
+    'Personal Website',
+    'Developer',
+    'Web Developer',
+    'Software Developer',
+    'Careers',
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://andrienw.com/careers',
+    title: 'Andrien Wiandyano — Careers',
+    description:
+      "Andrien Wiandyano's life journey, education, and professional experience.",
+    siteName: 'Andrien Wiandyano — Careers',
+  },
 };
 
 export default function Page() {
